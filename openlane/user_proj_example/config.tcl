f655275421711a2bd1394c43d77747ad07b4acd2
Tanvir Arafin <mdtanvir.arafin@morgan.edu>
2022-06-16 04:15:45 -0700

    error

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -28,7 +28,7 @@ set ::env(DESIGN_IS_CORE) 0
 
 set ::env(CLOCK_PORT) "wb_clk_i"
 set ::env(CLOCK_NET) "counter.clk"
-set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PERIOD) "100"
 
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 900 600"
@@ -36,7 +36,7 @@ set ::env(DIE_AREA) "0 0 900 600"
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(PL_TARGET_DENSITY) 0.05
+set ::env(PL_TARGET_DENSITY) 0.25
 
 # Maximum layer used for routing is metal 4.
 # This is because this macro will be inserted in a top level (user_project_wrapper) 
