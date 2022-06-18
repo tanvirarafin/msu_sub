3adb06d043c67fd3f2bb6df045702cd8af09663f
Tanvir Arafin <mdtanvir.arafin@morgan.edu>
2022-06-17 21:46:48 -0700

    Updates

--- openlane/risc_spm/config.tcl
+++ openlane/risc_spm/config.tcl
@@ -4,20 +4,27 @@ set ::env(DESIGN_NAME) RISC_SPM
 # Change if needed
 set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/../../verilog/rtl/risc_spm/*.v]
 
-# Fill this
-# Fill this
+set ::env(DESIGN_IS_CORE) 0
+
+
 
 set ::env(CLOCK_PERIOD) "100.0"
 set ::env(CLOCK_PORT) "clk"
 
-set ::env(PL_TARGET_DENSITY) "0.52"
-set ::env(GLB_RT_ADJUSTMENT) "0.2"
-set ::env(FP_CORE_UTIL) "50"
+
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 1000 1000"
+
+set ::env(PL_BASIC_PLACEMENT) 1
+set ::env(PL_TARGET_DENSITY) 0.15
 
 
+set ::env(RT_MAX_LAYER) {met4}
 
-set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
-if { [file exists $filename] == 1} {
-	source $filename
-}
+# You can draw more power domains if you need to 
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
 
+set ::env(DIODE_INSERTION_STRATEGY) 4 
+
+set ::env(RUN_CVC) 1
